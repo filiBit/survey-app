dbc53f4a2168b593947287f9e60f6bd57f917d4d
Filip Biterski <filip.biterski@gmail.com>
2021-11-23 20:01:59 +0100

    Setup React, bundling, transpiling and linting

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = function App() {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './frontend/src/index.tsx',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ],
+  devServer: {
+    static: './dev-build',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { useBuiltIns: 'entry' }], '@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
+    ],
+  },
+};
