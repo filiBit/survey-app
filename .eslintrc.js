7a5c9133e703bb834cc60eb114f141354b956082
Filip Biterski <filip.biterski@gmail.com>
2021-12-06 00:01:55 +0100

    Update format and lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,29 +5,24 @@ module.exports = {
     es2021: true,
     node: true
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 13,
-    sourceType: "module"
+    sourceType: 'module'
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   settings: {
     react: {
-      version: "detect"
+      version: 'detect'
     }
   },
-  plugins: ["react-hooks", "@typescript-eslint"],
+  plugins: ['react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
-    "@typescript-eslint/no-var-requires": "off",
-    "prettier/prettier": "error",
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    '@typescript-eslint/no-var-requires': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': 'error'
   }
 }
